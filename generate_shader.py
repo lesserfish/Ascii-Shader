298eb9b28aa0023b59f8c4e3a8a29b43a47b900d
vchavauty <v.chavauty@gmail.com>
2021-06-28 09:55:23 -0400

    first commit

---
+++ generate_shader.py
@@ -0,0 +1,83 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+f = open("charlist", "r");
+
+
+def content(c, name = ""):
+    image = Image.new("RGBA", (16, 16), (1, 1, 1));
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("./arial.ttf", 12)
+    draw.text((4, 0), c, font=font)
+    function_content = "return ("
+    
+    contstatus = False;
+    
+    for i in range(0 , 16):
+        white = False
+        startingpoint = 0;
+        endingpoint = 0;
+        for j in range(0, 16):
+            coordinate = i, j
+            p = image.getpixel(coordinate);
+            v = p[0]
+            if(v > 110): #If this is White
+                if(white == False): #We just got in a white tile
+                    white = True
+                    startingpoint = j
+                    endingpoint = j
+                else:
+                    endingpoint = j;
+            else: # This is a black Tile
+                if(white == True):
+                    endingpoint = j;
+                    white = False;
+                    if(contstatus == True): 
+                        function_content = function_content + "+"
+                    contstatus = True
+                    function_content = function_content + "float(coord.x >= " + str(i)+".0&& coord.x <= " + str(i+1)+".0 && coord.y >= " + str(startingpoint) + ".0&& coord.y <= " + str(endingpoint) + ".0)";
+            if(j == 15 and white == True): #We have finished and
+                endingpoint = j;
+                white = False;
+                if(contstatus == True):
+                    function_content = function_content + "+"
+                contstatus = True
+                function_content = function_content + "float(coord.x >= " + str(i)+".0&& coord.x <= " + str(i+1)+".0 && coord.y >= " + str(startingpoint) + ".0&& coord.y <= " + str(endingpoint) + ".0)";function_content = function_content
+    
+    if(contstatus == False):
+        function_content = """return vec4(0.0);"""
+    else:
+        function_content = function_content + ") * COLOR;";
+    image.save("png/{}.png".format(name))
+    return str(function_content)
+
+
+SHADER = """#define resolutioni 16\n#define resolution float(resolutioni)\n#define COLOR vec4(1.0, 1.0, 1.0, 1.0)\n""";
+DEFINE_HEADER = ""
+FUNCTION_EXPLICIT = ""
+i = 0
+charlist = "const int char_list[] = int[](char_everything, ";
+getchar_function = "vec4 o = "
+for line in f:
+    _char = line[0]
+    funcname = line[2:].rstrip()
+    define = "#define char{} {}".format(funcname, str(i))
+    funccontent = content(_char, funcname) 
+    function = """vec4 """ + funcname + """(vec2 coord){"""+ funccontent  +"""} """;
+    DEFINE_HEADER = DEFINE_HEADER + "\n" + define
+    FUNCTION_EXPLICIT = FUNCTION_EXPLICIT + "\n" + function
+    charlist = charlist + "char" + funcname + ", ";
+    getchar_function = getchar_function + "cchar == char" + funcname + "?" + funcname + "(coord) :\n\t\t\t"
+    i = i + 1;
+
+DEFINE_HEADER = DEFINE_HEADER + "\n#define char_everything {}".format(str(i))
+FUNCTION_EXPLICIT = FUNCTION_EXPLICIT + "\nvec4 _everything(vec2 coord){return vec4(1.0);}\n"
+clist = charlist[0: len(charlist) - 2] + ");"
+getchar_function = getchar_function + "cchar == char_everything?_everything(coord) : \nvec4(0.0);"
+SHADER = SHADER + DEFINE_HEADER + "\n" + FUNCTION_EXPLICIT + "\n" + clist + "\n" +  """vec4 getchar(int pos, vec2 coord)\n{\nint cchar = pos < char_list.length() ? char_list[pos] : 500;\n"""+ getchar_function + "\nreturn o; \n}" + """void mainImage( out vec4 fragColor, in vec2 fragCoord )\n{\nint pos = int(fragCoord.y / resolution)*int(iResolution.x / resolution) + int(fragCoord.x / resolution);\nvec2 coord = fragCoord - vec2(resolutioni * int(fragCoord.x / resolution), resolutioni*int(fragCoord.y / resolution));\ncoord.y = 16.0 - coord.y;\nfragColor = getchar(pos, coord);\n}"""
+
+sh = open("shader.glsl", "w")
+sh.write(SHADER);
+sh.close()
+f.close()
