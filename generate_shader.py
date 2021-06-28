3b7ff62a4eae96d7ddc0901bb91e6d75effaae4b
vchavauty <v.chavauty@gmail.com>
2021-06-28 18:59:52 -0400

    Added possiblity to change font and resolution more easily! Recreated with res 14 x 14 and font 10

--- generate_shader.py
+++ generate_shader.py
@@ -4,25 +4,27 @@ from PIL import ImageFont
 
 f = open("charlist", "r");
 
+RESOLUTION = 14
+FONT_SIZE = 10
 
 def content(c, name = ""):
-    image = Image.new("RGBA", (16, 16), (1, 1, 1));
+    image = Image.new("RGBA", (RESOLUTION, RESOLUTION), (1, 1, 1));
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("./arial.ttf", 12)
-    draw.text((4, 0), c, font=font)
+    font = ImageFont.truetype("./arial.ttf", FONT_SIZE)
+    draw.text((2, 0), c, font=font)
     function_content = "return ("
     
     contstatus = False;
     
-    for i in range(0 , 16):
+    for i in range(0 , RESOLUTION):
         white = False
         startingpoint = 0;
         endingpoint = 0;
-        for j in range(0, 16):
+        for j in range(0, RESOLUTION):
             coordinate = i, j
             p = image.getpixel(coordinate);
             v = p[0]
-            if(v > 110): #If this is White
+            if(v > 90): #If this is White
                 if(white == False): #We just got in a white tile
                     white = True
                     startingpoint = j
@@ -37,7 +39,7 @@ def content(c, name = ""):
                         function_content = function_content + "+"
                     contstatus = True
                     function_content = function_content + "float(coord.x >= " + str(i)+".0&& coord.x <= " + str(i+1)+".0 && coord.y >= " + str(startingpoint) + ".0&& coord.y <= " + str(endingpoint) + ".0)";
-            if(j == 15 and white == True): #We have finished and
+            if(j == RESOLUTION - 1 and white == True): #We have finished and
                 endingpoint = j;
                 white = False;
                 if(contstatus == True):
@@ -53,7 +55,7 @@ def content(c, name = ""):
     return str(function_content)
 
 
-SHADER = """#define resolutioni 16\n#define resolution float(resolutioni)\n#define COLOR vec4(1.0, 1.0, 1.0, 1.0)\n""";
+SHADER = """#define resolutioni """ +str(RESOLUTION)+"""\n#define resolution float(resolutioni)\n#define COLOR vec4(1.0, 1.0, 1.0, 1.0)\n#define REPEAT true\n""";
 DEFINE_HEADER = ""
 FUNCTION_EXPLICIT = ""
 i = 0
@@ -75,7 +77,7 @@ DEFINE_HEADER = DEFINE_HEADER + "\n#define char_everything {}".format(str(i))
 FUNCTION_EXPLICIT = FUNCTION_EXPLICIT + "\nvec4 _everything(vec2 coord){return vec4(1.0);}\n"
 clist = charlist[0: len(charlist) - 2] + ");"
 getchar_function = getchar_function + "cchar == char_everything?_everything(coord) : \nvec4(0.0);"
-SHADER = SHADER + DEFINE_HEADER + "\n" + FUNCTION_EXPLICIT + "\n" + clist + "\n" +  """vec4 getchar(int pos, vec2 coord)\n{\nint cchar = pos < char_list.length() ? char_list[pos] : 500;\n"""+ getchar_function + "\nreturn o; \n}" + """void mainImage( out vec4 fragColor, in vec2 fragCoord )\n{\nint pos = int(fragCoord.y / resolution)*int(iResolution.x / resolution) + int(fragCoord.x / resolution);\nvec2 coord = fragCoord - vec2(resolutioni * int(fragCoord.x / resolution), resolutioni*int(fragCoord.y / resolution));\ncoord.y = 16.0 - coord.y;\nfragColor = getchar(pos, coord);\n}"""
+SHADER = SHADER + DEFINE_HEADER + "\n" + FUNCTION_EXPLICIT + "\n" + clist + "\n" +  """vec4 getchar(int pos, vec2 coord)\n{\nint cchar = pos < char_list.length() ? char_list[pos] : REPEAT ? int(mod(float(pos), float(char_list.length()))) : 500;\n"""+ getchar_function + "\nreturn o; \n}" + """void mainImage( out vec4 fragColor, in vec2 fragCoord )\n{\nint pos = int(fragCoord.y / resolution)*int(iResolution.x / resolution) + int(fragCoord.x / resolution);\nvec2 coord = fragCoord - vec2(resolutioni * int(fragCoord.x / resolution), resolutioni*int(fragCoord.y / resolution));\ncoord.y = 16.0 - coord.y;\nfragColor = getchar(pos, coord);\n}"""
 
 sh = open("shader.glsl", "w")
 sh.write(SHADER);
